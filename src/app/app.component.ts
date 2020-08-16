2503f0727ea6ea8e46a7216f87ec2a7f7470ed14
Benjamin Po <benjaminpo212@gmail.com>
2020-08-16 19:53:21 +0800

    Tabs and Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-material-tab-router';
+  navLinks: any[];
+  activeLinkIndex = -1;
+  constructor(private router: Router) {
+    this.navLinks = [
+      {
+        label: 'First',
+        link: './first',
+        index: 0
+      }, {
+        label: 'Second',
+        link: './second',
+        index: 1
+      }, {
+        label: 'Third',
+        link: './third',
+        index: 2
+      },
+    ];
+  }
+  ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+    });
+  }
 }
